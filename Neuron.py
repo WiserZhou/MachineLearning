f265380b40a19080e52c4375adde67a4381e8778
WiserZhou <wiserx@qq.com>
2023-08-30 08:32:43 +0800

    add a neuron

--- Neuron.py
+++ Neuron.py
@@ -20,3 +20,47 @@ n = Neuron(weights, bias)
 
 x = np.array([2, 3])       # x1 = 2, x2 = 3
 print(n.feedforward(x))    # 0.9990889488055994
+
+# ... code from previous section here
+
+class OurNeuralNetwork:
+  '''
+  A neural network with:
+    - 2 inputs
+    - a hidden layer with 2 neurons (h1, h2)
+    - an output layer with 1 neuron (o1)
+  Each neuron has the same weights and bias:
+    - w = [0, 1]
+    - b = 0
+  '''
+  def __init__(self):
+    weights = np.array([0, 1])
+    bias = 0
+
+    # The Neuron class here is from the previous section
+    self.h1 = Neuron(weights, bias)
+    self.h2 = Neuron(weights, bias)
+    self.o1 = Neuron(weights, bias)
+
+  def feedforward(self, x):
+    out_h1 = self.h1.feedforward(x)
+    out_h2 = self.h2.feedforward(x)
+
+    # The inputs for o1 are the outputs from h1 and h2
+    out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+
+    return out_o1
+
+network = OurNeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x)) # 0.7216325609518421
+
+
+def mse_loss(y_true, y_pred):
+  # y_true and y_pred are numpy arrays of the same length.
+  return ((y_true - y_pred) ** 2).mean()
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+print(mse_loss(y_true, y_pred)) # 0.5
